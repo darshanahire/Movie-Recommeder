fbb423632984e2b7bdd1695b494894d11b1429e0
Darshan Ahire <72602965+darshanahire@users.noreply.github.com>
2023-08-03 12:55:15 +0530

    deployment commit last

--- api/app.py
+++ api/app.py
@@ -1,22 +1,63 @@
-# Importing flask module in the project is mandatory
-# An object of Flask class is our WSGI application.
-from flask import Flask
+import pandas as pd
+from flask import Flask, jsonify
+from flask.helpers import send_from_directory
+from flask_cors import CORS, cross_origin
+import requests
 
-# Flask constructor takes the name of
-# current module (__name__) as argument.
-app = Flask(__name__)
+newMovies = pd.read_pickle('movies.pkl')
+similarity = pd.read_pickle('similarity.pkl')
 
-# The route() function of the Flask class is a decorator,
-# which tells the application which URL should call
-# the associated function.
+def getMovieDetaile(movieId):
+    response = requests.get('https://api.themoviedb.org/3/movie/{}?api_key=2144c30ff91ce4ad919d206c68ffe29c'.format(movieId))
+    data = response.json()
+    return data
+
+def recomend(movie):
+    movie = movie.lower()
+    movie = movie.replace(r' ','-')
+    currMovie = newMovies[newMovies['comp'] == movie]
+    res = []
+    if(currMovie.size==0):
+        res.append("No Movies Found")
+        result = {'found' : 0,'data':None}
+    else:
+        movie_index = currMovie.index[0]
+        distance = similarity[movie_index]
+        res.append(getMovieDetaile(newMovies.iloc[movie_index].movie_id))
+        movies_list = sorted(list(enumerate(distance)), reverse=True, key = lambda x:x[1])[1:6]
+        for i in movies_list:
+            res.append(getMovieDetaile(newMovies.iloc[i[0]].movie_id))
+        result = {'found' : 1,'data':res}
+    return jsonify(result)
+
+
+# app = Flask(__name__)
+app = Flask(__name__, static_folder='../movie-recommendation-system/build', static_url_path='/')
+CORS(app)
+
+@app.route('/recommend/<movie>',methods=['GET'])
+@cross_origin()
+def Recommended(movie):
+    # print(movie)
+    return recomend(movie)
+
+@app.errorhandler(404)
+@cross_origin()
+def not_found(e):
+    return app.send_static_file('index.html')
+
+
 @app.route('/')
-# ‘/’ URL is bound with hello_world() function.
-def hello_world():
-	return 'Hello World'
+@cross_origin()
+def index():
+    return app.send_static_file('index.html')
 
-# main driver function
-if __name__ == '__main__':
+if __name__ == "__main__":
+    app.run(debug=True)
 
-	# run() method of Flask class runs the application
-	# on the local development server.
-	app.run()
+
+# print(recomend('falcon Rising'))
+
+
+
+# https://api.themoviedb.org/3/movie/65?api_key=2144c30ff91ce4ad919d206c68ffe29c
