8506156877da5c59b87e3ca2ede2898600edc469
Darshan Ahire <72602965+darshanahire@users.noreply.github.com>
2023-08-05 15:24:58 +0530

    deployment

--- api/app.py
+++ api/app.py
@@ -1,46 +1,22 @@
-import pandas as pd
-from flask import Flask, jsonify
-from flask.helpers import send_from_directory
-# from flask_cors import CORS, cross_origin
-import requests
+from flask import Flask
+from flask_cors import CORS, cross_origin
+from website import recomend;
 
-# app = Flask(__name__)
 app = Flask(__name__, static_folder='../movie-recommendation-system/build', static_url_path='/')
-# CORS(app)
+CORS(app)
 
 @app.route('/recommend/<movie>',methods=['GET'])
-# @cross_origin()
+@cross_origin()
 def Recommended(movie):
-    newMovies = pd.read_pickle('movies.pkl')
-    similarity = pd.read_pickle('similarity.pkl')
-    movie = movie.lower()
-    movie = movie.replace(r' ','-')
-    currMovie = newMovies[newMovies['comp'] == movie]
-    res = []
-    if(currMovie.size==0):
-        res.append("No Movies Found")
-        result = {'found' : 0,'data':None}
-    else:
-        movie_index = currMovie.index[0]
-        distance = similarity[movie_index]
-        response = requests.get('https://api.themoviedb.org/3/movie/{}?api_key=2144c30ff91ce4ad919d206c68ffe29c'.format(newMovies.iloc[movie_index].movie_id))
-        d = response.json()
-        res.append(d)
-        movies_list = sorted(list(enumerate(distance)), reverse=True, key = lambda x:x[1])[1:6]
-        for i in movies_list:
-            response = requests.get('https://api.themoviedb.org/3/movie/{}?api_key=2144c30ff91ce4ad919d206c68ffe29c'.format(newMovies.iloc[i[0]].movie_id))
-            d = response.json()
-            res.append(d)
-        result = {'found' : 1,'data':res}
-    return jsonify(result)
+    return recomend(movie)
 
 @app.errorhandler(404)
-# @cross_origin()
+@cross_origin()
 def not_found(e):
     return app.send_static_file('index.html')
 
 @app.route('/')
-# @cross_origin()
+@cross_origin()
 def index():
     return app.send_static_file('index.html')
 
---
+++ api/website/__init__.py
@@ -0,0 +1,29 @@
+import pandas as pd
+from flask import jsonify
+import requests
+
+newMovies = pd.read_pickle('movies.pkl')
+similarity = pd.read_pickle('similarity.pkl')
+
+def getMovieDetaile(movieId):
+    response = requests.get('https://api.themoviedb.org/3/movie/{}?api_key=2144c30ff91ce4ad919d206c68ffe29c'.format(movieId))
+    data = response.json()
+    return data
+
+def recomend(movie):
+    movie = movie.lower()
+    movie = movie.replace(r' ','-')
+    currMovie = newMovies[newMovies['comp'] == movie]
+    res = []
+    if(currMovie.size==0):
+        res.append("No Movies Found")
+        result = {'found' : 0,'data':None}
+    else:
+        movie_index = currMovie.index[0]
+        distance = similarity[movie_index]
+        res.append(getMovieDetaile(newMovies.iloc[movie_index].movie_id))
+        movies_list = sorted(list(enumerate(distance)), reverse=True, key = lambda x:x[1])[1:6]
+        for i in movies_list:
+            res.append(getMovieDetaile(newMovies.iloc[i[0]].movie_id))
+        result = {'found' : 1,'data':res}
+    return jsonify(result)
